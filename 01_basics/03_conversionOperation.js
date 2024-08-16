00ad2432e001cc44fa774b70b8dbbc3d0a753450
Priyanka Dasila <88925116+Priyankaalina@users.noreply.github.com>
2024-08-16 17:57:32 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valInNumber = Number(score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber)
+
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1; false = 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "test" => true
+
+let someNum = 33
+
+let stringNum = String(someNum)
+console.log(typeof stringNum)
+console.log(stringNum)
